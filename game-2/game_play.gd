1acc4741ead99454cbc5ce094adb485d7211bedc
slippy-cup <lar9538@g.rit.edu>
2026-02-26 01:18:22 -0500

    Lineart displays!
    The line art display, but the UI is very funky

--- game-2/game_play.gd
+++ game-2/game_play.gd
@@ -1,23 +1,36 @@
 extends CanvasLayer
 
+@onready var coloringCanvas : Control = $ColoringCanvas
+@onready var lineArtLayer:CanvasLayer = $LineArt
+@onready var drawingContainer:Control = $LineArt/ColoringContainer
+
+var currentPage:int
+
 #can only put pictures in this array
 var coloringLayers: Array[Image] = [
-preload("res://coloring-images/catipillar.jpg").get_image(),
-preload("res://coloring-images/stringray.jpg").get_image()
+
 ]
 
 #will hold all the line art
-var pages: Array[Image] = []
+var pages: Array[Image] = [
+	
+	preload("res://coloring-images/catipillar.jpg").get_image(),
+	preload("res://coloring-images/stringray.jpg").get_image()
+]
 
 #size of the canvas, will be able to change it to mobile easily
 var canvasSize: Vector2 = Vector2(800, 600)
 
+var lineMask: Image
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	createPageDef()
+	drawPage(0)
 
 
 func createPageDef()-> void:
+
 	#creates an specific page for each line art coloring oage.
 	for i:int in range(pages.size()):
 		#This will create each new page, size, and that it contains red, blue and yellow 
@@ -25,6 +38,51 @@ func createPageDef()-> void:
 		img.fill(Color.TRANSPARENT)
 		coloringLayers.append(img)
 		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func setColoringLayer() -> void:
+	for c:TextureRect in coloringCanvas.get_children():
+		c.queue_free()
+		
+	var textRect:TextureRect = TextureRect.new()
+	textRect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	textRect.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	coloringCanvas.add_child(textRect)
+	#waits one frame to let the image load in
+	await get_tree().process_frame
+	textRect.name = "ColoringLayer"
+	updateColorDisplay()
+	#makes sure it's being rendered ontop 
+	lineArtLayer.layer = 1
+		
+
+func updateColorDisplay() -> void:
+	var display:TextureRect = coloringCanvas.get_node("ColoringLayer")
+	if display:
+		var text:ImageTexture = ImageTexture.new()
+		text.set_image(coloringLayers[currentPage])
+		display.texture = text
+		
+		
+func drawPage(pageNumber:int) -> void:
+	if pageNumber < 0 or pageNumber > pages.size():
+		return
+	currentPage = pageNumber
+	#clearDrawingContainer()
+	var img:Image = pages[currentPage]
+	img.convert(Image.FORMAT_RGBA8)
+	img.resize(int(canvasSize.x), int(canvasSize.y))
+	lineMask = img.duplicate()
+	
+	var text:ImageTexture = ImageTexture.new()
+	text = ImageTexture.create_from_image(img)
+	var textRect:TextureRect = TextureRect.new()
+	textRect.texture = text
+	textRect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	textRect.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	textRect.z_index = 0
+	await get_tree().process_frame
+	textRect.name = "LineArtImage"
+	drawingContainer.add_child(textRect)
+	
+	setColoringLayer()
+	
