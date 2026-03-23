2b5737136a0bbb8fcc1c90cea7b9b57aafe776f8
Henrybcool <henryfehlner5@gmail.com>
2026-03-23 13:51:25 -0400

    Started painting struct

---
+++ game-2/c#-scripts/PaintingStruct.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+// This is the struct for a painting
+// Should contain:
+//		Image to paint
+//		Required colors palette
+//		GUID for creating a filepath for unique photo storage
+[GlobalClass]
+public partial class PaintingStruct : Resource
+{
+	
+}
